5d086434546a0ebbd6e2bf4b2abb31eb823b00d0
kirilldechko <100310727+kirilldechko@users.noreply.github.com>
2024-02-28 11:32:18 +0300

    Бинарный поиск

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+@
+binary_search.py,f\e\fe69f8ae71553e6cd7b6e2c2302338459d32e755
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+Å
+Q.idea/sonarlint/securityhotspotstore/f/e/fe69f8ae71553e6cd7b6e2c2302338459d32e755,4\1\414182af2e356032c82992f2a562728530f3c12b
+^
+..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
---
+++ binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(my_list, item):
+    low_my_list = 0  # нажний порог поиска
+    height_my_list = len(my_list) - 1  # верхний порог, длинна списка минус 1
+    while low_my_list < height_my_list:  # пока верно условие выполнять >
+        mid = int((low_my_list + height_my_list) / 2)  # делим весь список на 2 (Бинарный поиск)
+        answer = my_list[mid]  # ответ равен значению под индексом равным mid
+        if mid == item:  # если mid == item
+            return answer  # выводить ответ == answer
+        if mid > item:  #  если середина списка > чем item
+            height_my_list = mid - 1  # конец списка равен середина списка - 1
+        else:
+            low_my_list = mid + 1  # если середина списка < чем item то начало списка равено середина списка + 1
+    return None
+
+
+test_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
+             "V", "W", "X", "Y", "Z"]
+
+print(binary_search(test_list, 6))
+
